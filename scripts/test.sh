dedb910eb21c27c39aafe8a848180ef20d21a006
Victor Martynov <vicmart143@gmail.com>
2021-11-02 18:21:06 +0700

    changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 npm ci --silent
 npm run build --silent
-TEST_RESULTT=$(npx jest --silent 2>&1 | tail -5 | tr -s ":" " " | tr -s "\n" " " )
+TEST_RESULT=$(npm run test 2>&1 | tr "\\\\\\\\" "/"| tr -s "\n" " ")
 TAG_ACTUAL=$(git tag --sort version:refname | tail -1 | head -n1)
 
 UNIQUE="VictorMarty11"
@@ -25,13 +25,13 @@ TEST_RESULT="TEST"
   TASK_ID=$(echo "$SEARCH_TASK" | jq -r ".[0].id")
 
   ADD_COMMENT=$(
-    curl -so dev/null -w '%{http_code}' -X POST "https://api.tracker.yandex.net/v2/issues/"$TASK_ID"/comments" \
+    curl --location --silent --request POST "https://api.tracker.yandex.net/v2/issues/"$TASK_ID"/comments" \
     --header 'Authorization: OAuth '"$TOKEN" \
     --header 'X-Org-ID: '"$ORG_ID" \
     --header 'Content-Type: application/json' \
-    --data-raw '{
-      "text": "some text"
-  }'
+    --data '{
+        "text":"'"$TEST_RESULT"'"
+    }'
 )
 
 echo "$ADD_COMMENT"
