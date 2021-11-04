787a3691f3b8324297dc3b344f56f3ef14e5bb07
Iuliia Saltrukovich <yulya_9060@mail.ru>
2021-11-04 17:16:10 +0300

    test7

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,7 +15,8 @@ SEARCH_TASK=$(
   --data-raw '{"filter":{"unique":"'"$UNIQUE"''"$TAG_ACTUAL"'"}}'
   )
 
-TASK_ID=$(echo "$SEARCH_TASK" | sed -z 's/\n/\\n/g' | jq ".[0].id")
+TASK_ID=$(echo "$SEARCH_TASK" | tr '\n''' | jq '.[0].id' 
+# TASK_ID=$(echo "$SEARCH_TASK" | jq -r ".[0].id")
 # DESCRIPTION1=$(echo "$SEARCH_TASK" | jq -r ".[0].description")
 DESCRIPTION1="test yu"
 echo "SEARCH_TASK $SEARCH_TASK"
