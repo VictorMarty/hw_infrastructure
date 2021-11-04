9521e5c7b371441b9cb822c075ff5577b47875d8
Iuliia Saltrukovich <yulya_9060@mail.ru>
2021-11-04 14:50:37 +0300

    LOADINGGGG

--- src/client/pages/Catalog.tsx
+++ src/client/pages/Catalog.tsx
@@ -22,7 +22,7 @@ export const Catalog: React.FC = () => {
                 <ProductItem product={p} />
             </div>
         )) :
-        'LOADING';
+        'LOADING111';
 
     return (
         <div className={bem()}>
