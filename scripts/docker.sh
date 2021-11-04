a5457d24f89412abb76e3318e80cf631e0aa9783
Victor Martynov <vicmart143@gmail.com>
2021-11-04 23:32:56 +0700

    new release

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -27,7 +27,7 @@ SEARCH_TASK=$(
 COMMENT="Docker: release:$TAG_ACTUAL"
 
 TASK_ID=$(echo "$SEARCH_TASK"  | tr '\r\n' ' ' | jq -r ".[0].id")
-DESCRIPTION=$(echo "$SEARCH_TASK"  | tr '\r\n' '\\n' | jq -r ".[0].description")
+DESCRIPTION=$(echo "$SEARCH_TASK"  | tr '\r\n' ' ' | jq -r ".[0].description")
 SUMMARY=$(echo "$SEARCH_TASK"  | tr '\r\n' ' ' | jq -r ".[0].summary" |  sed -z 's/\n/\\n/g')
 DESCRIPTION=$(echo "$DESCRIPTION"  | tr '\r\n' ' ' | sed -z 's/\n/\\n/g')
 NEW_DESCRIPTION="$DESCRIPTION""\n ""\n ""$COMMENT"" ""\n ""$MESSAGE"
